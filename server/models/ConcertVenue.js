f641dcbee709fb7b5d1c221b54a72d50e4a46cc9
dovakeen118 <kegillis118@gmail.com>
2021-03-17 12:24:07 -0400

    finished code

--- server/models/ConcertVenue.js
+++ server/models/ConcertVenue.js
@@ -20,7 +20,13 @@ class ConcertVenue {
 
       //get the results
       const concertVenueData = result.rows
+      // console.log("ORIGINAL DATA")
+      // console.log(concertVenueData)
+      
       const concertVenues = concertVenueData.map(concertVenue => new this(concertVenue))
+      
+      // console.log("VENUE OBJECT DATA")
+      // console.log(concertVenues)
 
       //release the connection back to the pool
       client.release()
@@ -29,6 +35,39 @@ class ConcertVenue {
     } catch(err) {
       console.log(err)
       throw(err)
+    }
+  }
+
+  async save() {
+    try {
+      const client = await pool.connect()
+      
+      // // Version 1
+      // let query = 'INSERT INTO concert_venues (name, location, capacity) VALUES '
+      // query += `('${this.name}', '${this.location}', '${this.capacity}');`
+      // await client.query(query)
+      
+      
+      // // Version 2: Placeholders
+      let query = 'INSERT INTO concert_venues (name, location, capacity) VALUES ($1, $2, $3)'
+      await client.query(query, [this.name, this.location, this.capacity])
+
+
+      const singleResult = await client.query("SELECT * FROM concert_venues ORDER BY ID DESC LIMIT 1")
+      // console.log(singleResult)
+
+      const persistedVenue = singleResult.rows[0]
+      // console.log(this)
+      // console.log(persistedVenue)
+
+      this.id = persistedVenue.id
+      
+      client.release()
+      
+      return true
+    } catch (error) {
+      console.error(error)
+      return false
     }
   }
 }
